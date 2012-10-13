8f1c16093d23299e935a664c49e0147a994a8436
Richie Zeng <richzeng@gmail.com>
2012-10-13 01:50:04 -0700

    added tests for lists and dicts

--- test_js_ast.py
+++ test_js_ast.py
@@ -8,6 +8,15 @@ class JavascriptASTTest(TestCase):
         module = Module(["test_statement1", "test_statement2"])
         self.assertEqual(str(module), "test_statement1\ntest_statement2\n")
 
+    def test_function(self):
+        function = Function("func", ["arg0", "arg1"], ["body0", "body1"])
+        self.assertEqual(str(function), "function func (arg0, arg1) {\n    body0;\n    body1;\n}")
+
+    def test_return(self):
+        return_node = Return("1")
+        print return_node
+        self.assertEqual(str(return_node), "return 1;")
+
     def test_if(self):
         if_node = If("a=b", ["func()"], ["func()"])
         self.assertEqual(str(if_node), "if (a=b) {\n    func();\n} else {\n    func();\n}")
@@ -24,6 +33,20 @@ class JavascriptASTTest(TestCase):
         call_node = Call("func", ["asdf", "asdf"])
         self.assertEqual(str(call_node), "func (asdf, asdf)")
 
+
+    def test_empty_dict(self):
+        dict_node = Dict([], [])
+        self.assertEqual(str(dict_node), "{}")
+
     def test_dict(self):
         dict_node = Dict(["key0", "key1"], ["val0", "val1"])
         self.assertEqual(str(dict_node), "{key0:val0, key1:val1}")
+
+
+    def test_empty_list(self):
+        list_node = List([])
+        self.assertEqual(str(list_node), "[]")
+
+    def test_list(self):
+        list_node = List(["a", "b", "c"])
+        self.assertEqual(str(list_node), "[a, b, c]")
