304bf12343530ac965e92867736fc1e70758097b
Sumukh Sridhara <sumukh@sumukhsridhara.com>
2012-10-13 12:44:45 -0700

    assets

--- server.py
+++ server.py
@@ -6,16 +6,19 @@ from flask import Flask,url_for,render_template
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/api", methods=['GET', 'POST'])
 def hello():
     contents = request.args['code'];
+    response.headers['Access-Control-Allow-Origin'] = origin
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return "eval({0})".format(str(request.args['code']).strip());
     """ 
     t = ast.parse(str(request.args['code']))
     output = str(convert(t))
     return str(request.args['callback'] + "({0})".format(output[:-2]))  
     """
-@app.route("/demo", methods=['GET', 'POST']) 
+@app.route("/", methods=['GET', 'POST']) 
 def test():
     return render_template('view.html')
 
