b801ed973132f801259bd4c670112ce1461bc66f
Sumukh Sridhara <sumukh@sumukhsridhara.com>
2012-10-13 14:32:14 -0700

    Server works!

--- server.py
+++ server.py
@@ -1,21 +1,33 @@
-from flask import Flask, request 
+from flask import Flask, request,jsonify
 import ast 
 from notebook import convert 
 from functools import wraps
 from flask import Flask,url_for,render_template
+import json
+import logging
 app = Flask(__name__)
 app.debug = True
+from flask import make_response
+from functools import update_wrapper
 
+def nocache(f):
+    def new_func(*args, **kwargs):
+        resp = make_response(f(*args, **kwargs))
+        resp.cache_control.no_cache = True
+        return resp
+    return update_wrapper(new_func, f)
 
+@nocache
 @app.route("/api", methods=['GET', 'POST'])
 def hello():
-    contents = request.args['code'];
-    return "eval({0})".format(str(request.args['code']).strip());
-    """ 
-    t = ast.parse(str(request.args['code']))
-    output = str(convert(t))
-    return str(request.args['callback'] + "({0})".format(output[:-2]))  
-    """
+	t = ast.parse(str(request.args['code']))
+	output = str(convert(t))
+	return jsonify(r=output)
+
+
+
+#return str(request.args['callback'] + "({0})".format(output[:-2]))  
+    
 @app.route("/", methods=['GET', 'POST']) 
 def test():
     return render_template('index.html')
