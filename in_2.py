85c73db56dca2133617ea21862f22042cda41553
Richie Zeng <richzeng@gmail.com>
2012-10-13 07:13:18 -0700

    fixed tabbing and added local variables

--- in_2.py
+++ in_2.py
@@ -1,7 +1,7 @@
 from pythonscript import js
 js('var py = require("./py")')
-js('console.log("Test in_2");')
+js('console.log("Test in_2")')
 def in_2():
     return 1 + 2
 print in_2()
-js('console.log("End test in_2");')
+js('console.log("End test in_2")')
---
+++ in_temp.py
@@ -0,0 +1,5 @@
+def temp():
+    x = 2
+    a = 1
+    for i in [1,2,3,4]:
+        print i
