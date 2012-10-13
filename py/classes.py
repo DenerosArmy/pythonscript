4de0118033c999eeebb06e8d712c17711e383a32
Nikita Kitaev <nikitakit@gmail.com>
2012-10-13 11:11:48 -0700

    Delete copy of tuple in classes.py

--- py/classes.py
+++ py/classes.py
@@ -13,44 +13,6 @@ f.print_x()
 
 # library functions
 
-class tuple(object):
-    def __init__(self, items):
-        self._items = items
-        self._len = -1
-
-    def __len__(self):
-        if self._len == -1:
-            count = 0
-            js("""for (var index in self._items) {
-                count += 1;
-            }
-            """)
-            self._len = count
-            return count
-        else:
-            return self._len
-
-    def __contains__(self, item):
-        js("""for (var index in self._items) {
-                  if (item === self._items[index]) {
-                      return true;
-                  }
-              }
-              """)
-        return False
-
-    def __iter__(self):
-        return self._items
-
-    def __str__(self):
-        res = "("
-        js("""for (var index in self._items) {
-                  res += '' + self._items[index] + ','
-              }
-              """)
-        res = res + ')' # TODO: why does += not work here?
-        return res
-
 t = (1, 2, 3)
 print "length of tuple", py.len(t)
 print "Tuple contains 2?", py.contains(t, 2)
