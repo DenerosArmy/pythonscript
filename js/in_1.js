63e8369f4323a77e9fa0d1d9023f489a53f8f275
Nikita Kitaev <nikitakit@gmail.com>
2012-10-13 11:38:03 -0700

    Don't commit js/ to the repo

--- js/in_1.js
+++
@@ -1,8 +0,0 @@
-py = require ("./py");
-console.log ("Test in_1");
-foo = py.def (py.kwargs, {"y":1}, function (x, y, kwargs) {
-    console.log (x + y);
-});
-foo ([], {"x":1, "y":2});
-console.log ("End test in_1");
-
--- js/in_2.js
+++
@@ -1,8 +0,0 @@
-py = require ("./py");
-console.log ("Test in_2");
-in_2 = py.def ({}, function () {
-    return 1 + 2;
-});
-console.log (in_2 ([], {}));
-console.log ("End test in_2");
-
